c9fab76dcb6c0db88bad20f941d6cb78394a0a3f
qhsdnr0 <qhsdnr0@gmail.com>
2021-10-11 18:56:58 +0900

    Add: users_view Create

---
+++ users/validation.py
@@ -0,0 +1,13 @@
+import re
+
+def validate_email(email):
+    email_pattern = '[a-zA-Z0-9\-_]+@[a-zA-Z0-9\-_]+.[a-zA-Z0-9\.\-_]+'
+    user_email = re.match(email_pattern, email)
+    if user_email != None:
+        return user_email.group()
+
+def validate_password(password):
+    password_pattern = '(?=.*[a-zA-Z0-9])(?=.*[0-9!@#\$\%\*\^])(?=.*[a-zA-Z!@#\$\%\*\^]).+'
+    user_password = re.match(password_pattern, password)
+    if user_password != None:
+        return user_password.group()
