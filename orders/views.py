e60c3dadf93e032d12fd2027c6aab02a0566141c
박현우 <pang@baghyeon-uui-MacBookAir.local>
2021-10-13 17:20:21 +0900

    Add: mlb_orders_view

--- orders/views.py
+++ orders/views.py
@@ -5,40 +5,43 @@ from django.views    import View
 from django.http     import JsonResponse
 
 from orders.models   import Order, Order_status_code
-from products.models import Product
+from products.models import Product, Products_sizes, Size
 from users.models    import User
 from utils           import login_required
 
 class OrderView(View):
+        @login_required
         def post(self, request):
             # try:
-                data           = json.loads(request.body)
+                data              = json.loads(request.body)
+                user              = request.user
+                type              = data['type']
+                value             = data['value']
+                quantity          = data['quantity']
+                products          = Product.objects.get(product_code = request.GET.get('code'))
+                products_filter   = Size.objects.get(type=type, value=value).products_sizes_set.filter(product = products)
 
-                products       = Product.objects.get(product_code = request.GET.get('code'))
-                products_sizes = products.products_sizes_set.all()
+                if products_filter.exists():
+                    
+                    products_filter.update(quantity=products_filter.first().quantity-int(quantity))
+                    products.number_of_selling += int(quantity)
+                    products.save()
+                    user.address = data['address']
+                    user.point = user.point - products.price
+                    user.save()
 
-                User.objects.create(
-                    name          = data['name'],
-                    mobile_number = data['mobile_number'],
-                    email         = data['email'],
-                    address       = data['address'],
-                )
+                    order_number = str(user.id)+str(request.GET.get('code')) +str(random.randrange(100000,999999))
+                    
+                    Order.objects.create(
+                        receiver_name           = data['receiver_name'],
+                        receiver_mobile_number  = data['receiver_mobile_number'],
+                        receiver_address        = data['receiver_address'],
+                        request                 = data['request'],
+                        order_number            = order_number,
+                        user_id                 = user.id
+                    )
 
-                user_id = User.objects.last().id
-                order_number = str(user_id)+ str(random.randrange(100000,999999))
-                
-                Order.objects.create(
-                    receiver_name           = data['receiver_name'],
-                    receiver_mobile_number  = data['receiver_mobile_number'],
-                    receiver_address        = data['receiver_address'],
-                    request                 = data['request'],
-                    order_number            = order_number,
-                    user_id                 = user_id
-                )
-
-                # if products_sizes.objects.filter()
-
-                return JsonResponse({"massage":"create"}, status=201)
+                    return JsonResponse({"massage":"create"}, status=201)
             
             # except KeyError:
             #     return JsonResponse({"massage":"KeyError"}, status=401)
