c61e74db88439a09c8adde6031039bbb762bade1
qhsdnr0 <qhsdnr0@gmail.com>
2021-10-18 10:40:09 +0900

    Fixed: products_models

--- products/models.py
+++ products/models.py
@@ -1,18 +1,22 @@
 from django.db    import models
+
 class Menu(models.Model):
     name = models.CharField(max_length=45)
     class Meta:
         db_table = "menus"
+
 class Category(models.Model):
     name = models.CharField(max_length=45)
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
     class Meta:
         db_table = "categories"
+
 class SubCategory(models.Model):
     name        = models.CharField(max_length=45)
     category    = models.ForeignKey(Category, on_delete=models.CASCADE)
     class Meta:
         db_table = "sub_categories"
+
 class Product(models.Model):
     name                = models.CharField(max_length=45)
     color               = models.CharField(max_length=45)
@@ -28,18 +32,21 @@ class Product(models.Model):
     category            = models.ForeignKey(Category, on_delete=models.SET_NULL , null=True)
     class Meta:
         db_table = "products"
+
 class Products_sizes(models.Model):
     quantity  = models.IntegerField(default=1000)
     size      = models.ForeignKey('Size', on_delete=models.CASCADE)
     product   = models.ForeignKey('Product', on_delete=models.CASCADE)
     class Meta:
         db_table = "products_sizes"
+
 class Size(models.Model):
     type    = models.CharField(max_length=45)
     value   = models.CharField(max_length=45)
     product = models.ManyToManyField(Product, through=Products_sizes, through_fields=('size', 'product'))
     class Meta:
         db_table = "sizes"
+        
 class Image(models.Model):
     image_url      = models.CharField(max_length=2000)
     product        = models.ForeignKey(Product, on_delete=models.CASCADE)
